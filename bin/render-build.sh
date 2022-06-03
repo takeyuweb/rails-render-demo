9a14b572db95c84e2f3d090b71034e18cb44ba99
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2022-06-03 11:16:32 +0000

    update

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,11 +5,15 @@ set -o errexit
 echo "copying .env"
 if [ $IS_PULL_REQUEST = "true" ]; then
   ls -al /etc/secrets/.env.preview
+  cat /etc/secrets/.env.preview
   cp /etc/secrets/.env.preview .env
 else
   ls -al /etc/secrets/.env.production
+  cat /etc/secrets/.env.production
   cp /etc/secrets/.env.production .env
 fi
+
+cat .env
 
 bundle install
 bundle exec rails assets:precompile
