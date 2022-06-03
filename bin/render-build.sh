dc40a9fad7628fc656d2b4ce8670f72402bfb05f
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2022-06-03 12:50:02 +0000

    Update for Preview Environment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,14 @@
 
 set -o errexit
 
+# .env.production だと dotenv-rails が RAILS_ENV=productionの時に選択してしまうのでこの名前
+echo "copying .env"
+if [ $IS_PULL_REQUEST = "true" ]; then
+  cp dotenv-prev .env
+else
+  cp dotenv-prod .env
+fi
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
