d4529639d31ab37725edb256172f7863fca3c0ea
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2022-06-03 10:14:57 +0000

    Update for Preview Environment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,14 @@
 
 set -o errexit
 
+echo "copying .env"
+if [ $IS_PULL_REQUEST = "true" ]; then
+  cp .env.preview .env
+else
+  cp .env.production .env
+fi
+rm .env.*
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
